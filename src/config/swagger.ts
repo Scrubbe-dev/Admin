13861a96cd5d5f2375940f032aafb38dc7e6d742
chinkauchenna2021 <uchennachinka4@gmail.com>
2025-03-31 12:48:05 +0100

    fix; remove swagger header

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -2,28 +2,52 @@ import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Express } from "express";
 
+
+// const customCss = `
+//   .swagger-ui .topbar {
+//     display: none;
+//   }
+//   .swagger-ui .information-container {
+//     display: none;
+//   }
+//   .swagger-ui .scheme-container {
+//     display: none;
+//   }
+// `;
+
+const option = {
+    customCss:'.swagger-ui .topbar { display: none }',
+}
+
 const options: swaggerJSDoc.Options = {
+   
   definition: {
     openapi: "3.0.0",
     info: {
-      title: "SCRUBBE API Documentation",
-      version: "1.0.0",
-      description: "A complete documentation of scrubbe api",
+        title: "SCRUBBE API Documentation",
+        version: "1.0.0",
+        description: "A complete documentation of scrubbe api",
     },
     servers: [
-      {
-        url: "http://localhost:3000", 
-        description: "Local Development Server",
-      },
+        {
+            url: "http://localhost:3000", 
+            description: "Local Development Server",
+        },
     ],
-  },
-  apis: ["./src/modules/system/system.routes.ts"],
+},
+apis: ["./src/modules/system/system.routes.ts"],
 };
 
 
 const swaggerSpec = swaggerJSDoc(options);
 
 export function setupSwagger(app: Express) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec,option));
   console.log("✅ Swagger Docs available at: http://localhost:3000/api-docs");
 }
+
+
+
+
+// for Auth 
+// app.use('/api-docs', basicAuth({ users: { 'admin': 'password' } }), swaggerUi.serve, swaggerUi.setup(specs));
