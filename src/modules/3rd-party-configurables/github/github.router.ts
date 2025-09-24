15d0bdac6c5be7973baa14c1429a3ec5cf1b4dd3
chinkauchenna2021 <uchennachinka4@gmail.com>
2025-09-24 12:25:18 +0100

    FIX: GITHUB CALLBACK URLs

--- src/modules/3rd-party-configurables/github/github.router.ts
+++ src/modules/3rd-party-configurables/github/github.router.ts
@@ -45,7 +45,7 @@ githubRouter.get("/connect", authMiddleware.authenticate, (req, res, next) => {
 
 /**
  * @swagger
- * /api/v1/integrations/github/callback:
+ * /api/v1/integrations/github/callbacks/github:
  *   get:
  *     summary: GitHub OAuth callback
  *     description: >
@@ -73,7 +73,7 @@ githubRouter.get("/connect", authMiddleware.authenticate, (req, res, next) => {
  *         description: Failed to connect GitHub
  */
 // FIX: Changed from "/callback" to "/callbacks/github" to match your redirect URI
-githubRouter.get("/callback", (req, res, next) => {
+githubRouter.get("/callbacks/github", (req, res, next) => {
   githubController.handleOAuthCallback(req, res, next);
 });
 
